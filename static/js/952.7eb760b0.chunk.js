f28efb7a320abac8314b78207674a6f982b40def
Alandi1999 <alandiofficial@gmail.com>
2025-04-02 10:45:24 -0300

    Updates

---
+++ static/js/952.7eb760b0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcyber_play=self.webpackChunkcyber_play||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.7eb760b0.chunk.js.map
